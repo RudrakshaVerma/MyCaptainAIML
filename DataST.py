b246d3d9330df81c1d790258fae9989c3e62b81c
Rudraksha Verma <57012484+RudrakshaVerma@users.noreply.github.com>
2023-07-24 17:58:16 +0530

    Add files via upload
    Data Structures

---
+++ DataST.py
@@ -0,0 +1,19 @@
+# Define two sets
+E = {0, 2, 4, 6, 8}
+N = {1, 2, 3, 4, 5}
+
+# Union
+union_result = E.union(N)
+print("Union of E and N is", union_result)
+
+# Intersection
+intersection_result = E.intersection(N)
+print("Intersection of E and N is", intersection_result)
+
+# Difference
+difference_result = E.difference(N)
+print("Difference of E and N is", difference_result)
+
+# Symmetric Difference
+symmetric_difference_result = E.symmetric_difference(N)
+print("Symmetric difference of E and N is", symmetric_difference_result)
